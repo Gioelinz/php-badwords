157c1f5c80db130335da82156dc094f7884dc6b4
Gioelinz <gioelerunza.gr@gmail.com>
2022-02-24 15:02:28 +0100

    add bad word with get censored

--- index.php
+++ index.php
@@ -9,4 +9,30 @@ $paragraph = 'Il mio teacher mi ha detto che sono scarso';
 
 $paragraph_length = strlen($paragraph);
 
-echo "$paragraph Lunghezza pragrafo: $paragraph_length";
+$get_word = $_GET["word"];
+
+// str_replace(porzioneDaModificare, conCosa, stringa) - sostituisce una porzione di una stringa con un nuovo valore
+
+$paragraph_censured = str_replace($get_word, '***', $paragraph);
+$censured_length = strlen($paragraph_censured);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo $paragraph_censured ?></h1>
+    <h2><?php echo "Lunghezza paragrafo originale: $paragraph_length"; ?></h2>
+    <h2><?php echo "Lunghezza paragrafo censurato: $censured_length"; ?></h2>
+
+</body>
+
+</html>
